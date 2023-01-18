0b16edd1c79d9e6b3f7130e6eccdffe569ede55b
John Capovani <john@capovani.com>
2023-01-18 00:36:51 -0500

    Changed proxy back to original

--- src/features/sightings/sightingService.js
+++ src/features/sightings/sightingService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://squatch-watch-backend-deployment.onrender.com/api/sightings/'
+const API_URL = '/api/sightings/'
 
 //Create new sighting
 const createSighting = async (sightingData, token) => {
